2401ac99e89a8df511d22739007f3253d0a78ab0
metl <metl@localhost.localdomain>
2021-05-10 12:24:12 -0400

    super scrip

--- weaponx.sh
+++ weaponx.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo -e "\e[31mMake sure you run in your home directory.\e[0m"
+sleep 3
+git clone https://github.com/VundleVim/Vundle.vim.git
+git clone https://github.com/metlx/metlx-vim.git
 git clone https://github.com/metlx/dots
-cd dots
-echo -e "\e[90mSetting up .tmux_conf\e[0m"
-sleep 1
-mv .tmux.conf ~/
+echo -e "\e[90mSetting thins up...\e[0m"
+sleep 3
+mv ~/metlx-vim/.vim ~/
+mkdir ~/.vim/bundle
+mv ~/Vundle.vim ~/.vim/bundle/Vundle.vim
+mv ~/metlx-vim/.vimrc ~/
+mv ~/dots/.tmux.conf ~/
+echo -e "\e[31mCleaning up...\e[0m"
+sleep 3
+rm -rf ~/dots
+rm -rf ~/metlx-vim
 echo -e "\e[32mdone!\e[0m"
